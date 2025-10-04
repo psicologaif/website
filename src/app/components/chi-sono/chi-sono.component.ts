014acc2524f206cfd2d23be21b0197acbdb98f4b
danilopichilli <danyp987@gmail.com>
2025-10-04 19:22:50 +0200

    fix chi sono page

--- src/app/components/chi-sono/chi-sono.component.ts
+++ src/app/components/chi-sono/chi-sono.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Tooltip, TooltipModule } from 'primeng/tooltip';
 
@@ -9,25 +9,12 @@ import { Tooltip, TooltipModule } from 'primeng/tooltip';
   templateUrl: './chi-sono.component.html',
   styleUrls: ['./chi-sono.component.css'],
 })
-export class ChiSonoComponent implements OnInit {
-  @ViewChild('tooltip') tooltip!: Tooltip;
-  isMobile: boolean = false;
-  showTooltip: boolean = false;
+export class ChiSonoComponent {
+  showContent: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.isMobile =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      );
-  }
-
-  focusElement(event: MouseEvent): void {
-    event.stopPropagation();
-
-    if (this.isMobile) {
-      this.showTooltip = !this.showTooltip;
-    }
+  toggleContent(): void {
+    this.showContent = !this.showContent;
   }
 }
